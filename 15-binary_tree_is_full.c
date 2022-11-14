ca146a44be1cc929cc874ea6a109d02336fc782a
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-11-14 11:58:29 -0800

    Check binary tree accomodation

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,31 @@
+#include "binary_tree.h"
+
+/**
+ * if_full_recursive - Checks if a binary tree is full recursively
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 0 if tree is full, 1 otherwise
+ */
+int is_full_recursive(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL && tree->right == NULL ||
+		    (tree->left == NULL && tree->right != NULL) ||
+	    	    if_full_recursive(tree->left) == 0 || 
+    		    if_full_recursive(tree->right) == 0)
+				return (0);
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 0 if tree is NULL or not full, -1 otherwise
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree = NULL)
+		return (0);
+	return (is_full_recursive(tree));
+}
