8f176a1357a701df47dd04422255b7f79a7478ad
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-11-14 12:45:40 -0800

    uncle node find

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds uncle node in a binary tree
+ * @node: Pointer to the node to find the uncle of
+ * Return: NULL if node's NULL or has no uncle, pointer to uncle node otherwise
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULLL);
+	if (node->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
+}
