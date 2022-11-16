17f2a10385a78d7e3ac447dd362b1f99ae61e377
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-16 23:25:56 +0300

    replace semicolon with coma

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -22,7 +22,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t p = 0; k = 0;
+		size_t p = 0, k = 0;
 
 		p = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 		k = tree->right ? 1 + binary_tree_height(tree->right) : 1;
