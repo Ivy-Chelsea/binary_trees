90238282dbff92fb9cddc19756251c0e5e43e66f
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-16 23:43:32 +0300

    fix betty issues

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,8 +9,11 @@ int is_full_recursive(const binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
-		if ((tree->left != NULL && tree->right == NULL) || (tree->left == NULL && tree->right != NULL) || is_full_recursive(tree->left) == 0 ||  is_full_recursive(tree->right) == 0)
-		    	return (0);
+		if ((tree->left != NULL && tree->right == NULL) ||
+		    (tree->left == NULL && tree->right != NULL) ||
+		    is_full_recursive(tree->left) == 0 || 
+		    is_full_recursive(tree->right) == 0)
+			return (0);
 	}
 	return (1);
 }
