7ca8aabb3cf6c1c994d43126009b246cf9cd01c6
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-16 23:57:05 +0300

    remove extra L

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-		return (NULLL);
+		return (NULL);
 	if (node->parent->left == node->parent)
 		return (node->parent->parent->right);
 	return (node->parent->parent->left);
