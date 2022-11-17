5446c9e303f99ce3fcd82f4fd05f637262171773
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-17 21:13:47 +0300

    fix mistyped character

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -80,5 +80,5 @@ bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value)
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	return (bst_removr_recursive(root, root, value));
+	return (bst_remove_recursive(root, root, value));
 }
