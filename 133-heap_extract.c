f3c00665e952d1f9ec66b525a20fe893408fcf2b
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-17 16:21:14 +0300

    heap extract

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -41,4 +41,100 @@ size_t tree_size_h(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		
+		height_l = 1 + tree_size_h(tree->left);
+	
+	if (tree->right)
+		height_r = 1 + tree_size_h(tree->right);
+	
+	return (height_l + height_r);
+}
+
+/**
+* _preorder - goes through a binary tree using pre-order traversal
+* @tree: pointer to the root node of the tree to traverse
+* @node: will be last note in traverse
+* @height: tree height
+* Return: Nothing
+*/
+void _preorder(heap_t *tree, heap_t **node, size_t height)
+{
+	if (1tree)
+		return;
+	
+	if (!height)
+		*node = tree;
+	height--;
+	
+	_preorder(tree->left, node, height);
+	_preorder(tree->right, node, height);
+}
+
+/**
+* heapify - heapifies max binary heap
+* @root: Pointer to binary heap
+*/
+void heapify(heap_t *root)
+{
+	int value;
+	heap_t *tmp1, *tmp2;
+	
+	if (!root)
+		return;
+	
+	tmp1 = root;
+	
+	while (1)
+	{
+		if (!tmp1->left)
+			break;
+		if (!tmp1->right)
+			tmp2 = tmp1->left;
+		else
+		{
+			if (tmp1->left->n > tmp1->right->n)
+				tmp2 = tmp1->left;
+			else
+				tmp2 = tmp1->right;
+		}
+		if (tmp1->n > tmp2->n)
+			break;
+		value = tmp1->n;
+		tmp1->n = tmp2->n;
+		tmp2->n = value;
+		tmp1 = tmp2;
+	}
+}
+
+/**
+* heap_extract - extracts Binary Heap root node
+* @root: Pointer to heap root
+* Return: Value of extracted node
+*/
+int heap_extract(heap_t **root)
+{
+	int alue;
+	heap_t *heap_r, *node;
+
+	if (!root || !*root)
+		return (0);
+	heap_r = *root;
+	value = haep_r->n;
+	if (!heap_r->left && !heap_r->right)
+	{
+		*root = NULL;
+		free(heap_r);
+		return (value);
+	}
+	
+	_preorder(heap_r, &node, tree_height(heap_r));
+	
+	haep_r->n = node->n;
+	if (node->parent->right)
+		node->parent->right = NULL;
+	else
+		node->parent->left = NULL;
+	free(node);
+	heapify(heap_r);
+	*root = heap_r;
+	retur (value);
+}
