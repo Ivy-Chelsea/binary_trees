89b948c3686be2a297f47065789bbfcc70bfd2df
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-11-17 05:01:28 -0800

    array from heap

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,49 @@
+#include "binary_ttrees.h"
+
+/**
+ * tree_size - Measures sum of binary tree heights
+ * @tree: Pointer to roott node of tree to measure
+ * Return: Height o success, 0if tree is NULL
+ */
+size_t tree_size(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		height_l = 1 + tree_size(tree->left);
+
+	if (tree->right)
+		height_r = 1 + tree_size(tree->right);
+
+	return (height_l + height_r);
+}
+
+/**
+ * heap_to_sorted_array - Converts a Binary Heap to a sorted array
+ * @heap: Pointer to root node of heap to convert
+ * @size: Address to store array size
+ * Return: Pointer to arrya sorted in descending order
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	int i, *a = NULL;
+
+	if (!heap || !size)
+		return (NULL);
+
+	*size = tree_size(heap) + 1;
+
+	a = malloc(sizeof(int) * (*size));
+
+	if (!a)
+		return (NULL);
+
+	for (i = 0; heap; i++)
+		a[i] = heap_extract(&heap);
+
+	return (a);
+}
