e13143c27c031c15e9dffcb16e81ba1b2d5c439d
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-17 21:08:25 +0300

    fix moistyped characters

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -13,7 +13,7 @@ bst_t *bst_search(const bst_t *tree, int value)
 		if (tree->n == value)
 			return ((bst_t *)tree);
 		if (tree->n > value)
-			return (bst_search(tree->left, avlue));
+			return (bst_search(tree->left, value));
 		return (bst_search(tree->right, value));
 	}
 	return (NULL);
