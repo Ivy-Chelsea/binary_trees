a7bd56751e600181d76b722b9495217ea7ee18a0
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-11-17 04:00:57 -0800

    AVL tree recursion

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,61 @@
+#include "binary_trees.h"
+
+/**
+ * create_tree - Creates an AVL tree with recursion
+ * @node: Pointer node
+ * @array: input array of integers
+ * @size: array size
+ * @node: node to add to the left and right
+ * Return: nothing
+ */
+void create_tree(avl_t **node, int *array, size_t size, int node)
+{
+	size_t middle;
+
+	if (size == 0)
+		return;
+
+	middle = (size / 2);
+	middle = (size % 2 == 0) ? middle - 1 : middle;
+
+	if (mode == 1)
+	{
+		(*node)->left = binary_tree_node(*node, array[middle]);
+		create_tree(&((*node)->left), array, middle, 1);
+		create_tree(&((*node)->left), array + middle + 1, (size - 1 - middle), 2);
+	}
+	else
+	{
+		(*node)->right = binary_tree_node(*node, array[middle]);
+		create_tree(&((*node)->right), array, middle, 1);
+		create_tree(&((*node)->right), array + middle + 1, (size - 1 - middle), 2);
+	}
+}
+
+/**
+ * sorted_array_to_avl - creates root node that calls to create tree
+ * @array: input array of integers
+ * @size: size of array
+ * Return: Pointer to the root
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *root;
+	size_t middle;
+
+	root = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	middle = (size / 2);
+
+	middle = (size % 2 == 0) ? middle - 1 : middle;
+
+	root = binary_tree_node(root, array[middle]);
+
+	create_tree(&root, array, middle, 1);
+	create_tree(&root, array + middle + 1, (size - 1 - middle), 2);
+
+	return (root);
+}
