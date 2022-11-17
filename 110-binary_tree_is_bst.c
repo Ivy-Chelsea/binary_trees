fec2001832701d668a77eaad79984b36df342034
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-17 22:11:36 +0300

    fix betty issues

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -14,7 +14,8 @@ int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 	{
 		if (tree->n < lo || tree->n > hi)
 			return (0);
-		return (is_bst_helper(tree->left, lo, tree->n - 1) && is_bst_helper(tree->right, tree->n + 1, hi));
+		return (is_bst_helper(tree->left, lo, tree->n - 1)
+			&& is_bst_helper(tree->right, tree->n + 1, hi));
 	}
 	return (1);
 }
@@ -23,7 +24,7 @@ int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
  * binary_tree_is_bst - Checks if a binary tree is a valid binaryu search tree
  * @tree: Pointer to the root node of the tree to check
  * Return: 1 if tree is a valid BST, 0 otherwise
- * */
+ */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
