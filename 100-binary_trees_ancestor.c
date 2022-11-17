385602020de4f05eaa24312eaad27ce86cfc0e57
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-11-17 11:45:33 +0300

    fix betty issues

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,12 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_ancesstor - Finds the lowest ancestor of two nodes
+ * binary_trees_ancesstor - Finds the lowest ancestor of two nodes
  * @first: Pointer to the first node
  * @second: Pointer to the second node
  * Return: NULL if there are no common ancestors, Otherwise the common ancestor
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
 {
 	binary_tree_t *mom, *pop;
 
